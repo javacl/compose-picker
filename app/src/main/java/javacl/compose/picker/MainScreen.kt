c907a48ebda934292b23350f93d88353b8a4a64f
sadeghi <javacl.sadeghi@gmail.com>
2022-06-28 15:51:43 +0430

    created

--- app/src/main/java/javacl/compose/picker/MainScreen.kt
+++ app/src/main/java/javacl/compose/picker/MainScreen.kt
@@ -1,20 +1,17 @@
 package javacl.compose.picker
 
-import android.net.Uri
-import android.os.Environment
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.launch
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.core.content.FileProvider
-import java.io.File
 
 @Composable
 fun MainScreen() {
@@ -31,18 +28,11 @@ fun MainScreen() {
     )
 
     val cameraTakePictureLauncher = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.TakePicture(),
-        onResult = {
-            if (it)
+        contract = ActivityResultContracts.TakePicturePreview(),
+        onResult = { bitmap ->
+            bitmap?.let {
                 Toast.makeText(context, "Camera Take Picture Successful", Toast.LENGTH_SHORT).show()
-        }
-    )
-
-    val cameraTakeVideoLauncher = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.CaptureVideo(),
-        onResult = {
-            if (it)
-                Toast.makeText(context, "Camera Take Video Successful", Toast.LENGTH_SHORT).show()
+            }
         }
     )
 
@@ -56,34 +46,21 @@ fun MainScreen() {
         }) {
             Text(text = "Image From Gallery")
         }
+
         Spacer(modifier = Modifier.size(16.dp))
+
         Button(onClick = {
             galleryLauncher.launch("video/*")
         }) {
             Text(text = "Video From Gallery")
         }
-        Spacer(modifier = Modifier.size(16.dp))
-        Button(onClick = {
-            val photoFile = File.createTempFile(
-                "IMG_",
-                ".jpg",
-                context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-            )
 
-            val uri = FileProvider.getUriForFile(
-                context,
-                "${context.packageName}.provider",
-                photoFile
-            )
-            cameraTakePictureLauncher.launch(uri)
+        Spacer(modifier = Modifier.size(16.dp))
+
+        Button(onClick = {
+            cameraTakePictureLauncher.launch()
         }) {
             Text(text = "Camera Take Picture")
-        }
-        Spacer(modifier = Modifier.size(16.dp))
-        Button(onClick = {
-            // cameraTakeVideoLauncher.launch()
-        }) {
-            Text(text = "Camera Take Video")
         }
     }
 }
